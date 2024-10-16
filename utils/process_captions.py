032a994989d61047c61f2bd031902e5b8461ae78
hoangduyhieu <51811393+hoangduyhieu@users.noreply.github.com>
2024-10-16 22:07:07 +0700

    Update process_captions.py

--- utils/process_captions.py
+++ utils/process_captions.py
@@ -19,6 +19,9 @@ def download_captions(captions, download_folder_path, title_of_output_mp4, capti
                 srt_path = os.path.join(download_folder_path, srt_name)
                 srt_content = webvtt.read(vtt_path)
                 srt_content.save_as_srt(srt_path)
+              
+                # Remove VTT file
+                os.remove(vtt_path)
                 
         else:
             print("Only VTT captions are supported. Please create a github issue if you'd like to add support for other formats.")
